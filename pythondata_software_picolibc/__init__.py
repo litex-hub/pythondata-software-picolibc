ec8d33db522ce342ab0f81e673ef5e0503d91962
LiteX Robot <robot@timvideos.us>
2021-10-05 20:13:58 +0000

    Updating pythondata-software-picolibc to 0.0.post21344
    Updated data to v0.0-21239-g53567cf82 based on 53567cf82b92f1bd777ed57111232a110666b4df from https://github.com/picolibc/picolibc.
    > commit 53567cf82b92f1bd777ed57111232a110666b4df
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Mon Oct 4 13:00:45 2021 -0700
    >
    >     Use expr ':' instead of 'substr' operator
    >
    >     Mac OS X expr doesn't have the substr operator, so use regex with ':'
    >     instead.
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post105 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -2,46 +2,49 @@ import os.path
 __dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
 data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
+
 # Module version
-version_str = "0.0.post6206"
-version_tuple = (0, 0, 6206)
+version_str = "0.0.post21344"
+version_tuple = (0, 0, 21344)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post6206")
+    pversion = V("0.0.post21344")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post6140"
-data_version_tuple = (0, 0, 6140)
+data_version_str = "0.0.post21239"
+data_version_tuple = (0, 0, 21239)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post6140")
+    pdata_version = V("0.0.post21239")
 except ImportError:
     pass
-data_git_hash = "81fb4f00c2cfe13814765968e09931ffa93b5138"
-data_git_describe = "v0.0-6140-g81fb4f00c"
+data_git_hash = "53567cf82b92f1bd777ed57111232a110666b4df"
+data_git_describe = "v0.0-21239-g53567cf82"
 data_git_msg = """\
-commit 81fb4f00c2cfe13814765968e09931ffa93b5138
-Author: Chris Bieneman <beanz@apple.com>
-Date:   Fri Nov 6 23:19:29 2015 +0000
+commit 53567cf82b92f1bd777ed57111232a110666b4df
+Author: Keith Packard <keithp@keithp.com>
+Date:   Mon Oct 4 13:00:45 2021 -0700
 
-    [CMake] Need to filter ${arch}/*.c builtins as well as ${arch}/*.S builtins.
+    Use expr ':' instead of 'substr' operator
     
-    This was broken in r248542 when I refactored this to support builtins where ${arch} didn't match the directory prefix (i.e. armv7s).
+    Mac OS X expr doesn't have the substr operator, so use regex with ':'
+    instead.
     
-    git-svn-id: https://llvm.org/svn/llvm-project/compiler-rt/trunk@252365 91177308-0d34-0410-b5e6-96231b3b80d8
+    Signed-off-by: Keith Packard <keithp@keithp.com>
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post66"
-tool_version_tuple = (0, 0, 66)
+tool_version_str = "0.0.post105"
+tool_version_tuple = (0, 0, 105)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post66")
+    ptool_version = V("0.0.post105")
 except ImportError:
     pass
+
 
 def data_file(f):
     """Get absolute path for file inside pythondata_software_picolibc."""
