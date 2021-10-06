80a61e5b651e3be7035c027bd6053eb5dcb69d54
LiteX Robot <robot@timvideos.us>
2021-10-06 17:12:58 +0000

    Updating pythondata-software-picolibc to 0.0.post21347
    Updated data to v0.0-21239-g53567cf82 based on 53567cf82b92f1bd777ed57111232a110666b4df from https://github.com/picolibc/picolibc.
    > commit 53567cf82b92f1bd777ed57111232a110666b4df
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Mon Oct 4 13:00:45 2021 -0700
    >
    >     Use expr ':' instead of 'substr' operator
    >
    >     Mac OS X expr doesn't have the substr operator, so use regex with ':'
    >     instead.
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post108 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "0.0.post21344"
-version_tuple = (0, 0, 21344)
+version_str = "0.0.post21347"
+version_tuple = (0, 0, 21347)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post21344")
+    pversion = V("0.0.post21347")
 except ImportError:
     pass
 
@@ -37,11 +37,11 @@ Date:   Mon Oct 4 13:00:45 2021 -0700
 """
 
 # Tool version info
-tool_version_str = "0.0.post105"
-tool_version_tuple = (0, 0, 105)
+tool_version_str = "0.0.post108"
+tool_version_tuple = (0, 0, 108)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post105")
+    ptool_version = V("0.0.post108")
 except ImportError:
     pass
 
