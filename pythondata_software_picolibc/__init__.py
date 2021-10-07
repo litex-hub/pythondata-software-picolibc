0f339e9bf97bd4d0c998aee3098a3288022fa77e
LiteX Robot <robot@timvideos.us>
2021-10-07 08:47:05 +0000

    Updating pythondata-software-picolibc to 0.0.post21352
    Updated data to v0.0-21240-g4b4a69f42 based on 4b4a69f42f2e6fa5d6e44607adefe10f38d1b51a from https://github.com/picolibc/picolibc.
    > commit 4b4a69f42f2e6fa5d6e44607adefe10f38d1b51a
    > Author: Yasushi SHOJI <yashi@spacecubics.com>
    > Date:   Wed Oct 6 13:29:33 2021 +0900
    >
    >     meson.build: Remove unused POSIX_CONSOLE define from picolibc.h
    >
    >     The definition POSIX_CONSOLE in picolibc.h is not used at all in the
    >     tree.  In fact, it's never been used since it's introduced in
    >     the commit 91b5c92e.
    >
    >     The build time option 'posix-console' _is_ in use for checking against
    >     tinystdio and in newlib/libc/tinystdio/meson.build to enable building
    >     posixiob.c.  The option itself is, of course, unchanged.
    >
    >     Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,44 +4,49 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "0.0.post21347"
-version_tuple = (0, 0, 21347)
+version_str = "0.0.post21352"
+version_tuple = (0, 0, 21352)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post21347")
+    pversion = V("0.0.post21352")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post21239"
-data_version_tuple = (0, 0, 21239)
+data_version_str = "0.0.post21240"
+data_version_tuple = (0, 0, 21240)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post21239")
+    pdata_version = V("0.0.post21240")
 except ImportError:
     pass
-data_git_hash = "53567cf82b92f1bd777ed57111232a110666b4df"
-data_git_describe = "v0.0-21239-g53567cf82"
+data_git_hash = "4b4a69f42f2e6fa5d6e44607adefe10f38d1b51a"
+data_git_describe = "v0.0-21240-g4b4a69f42"
 data_git_msg = """\
-commit 53567cf82b92f1bd777ed57111232a110666b4df
-Author: Keith Packard <keithp@keithp.com>
-Date:   Mon Oct 4 13:00:45 2021 -0700
+commit 4b4a69f42f2e6fa5d6e44607adefe10f38d1b51a
+Author: Yasushi SHOJI <yashi@spacecubics.com>
+Date:   Wed Oct 6 13:29:33 2021 +0900
 
-    Use expr ':' instead of 'substr' operator
+    meson.build: Remove unused POSIX_CONSOLE define from picolibc.h
     
-    Mac OS X expr doesn't have the substr operator, so use regex with ':'
-    instead.
+    The definition POSIX_CONSOLE in picolibc.h is not used at all in the
+    tree.  In fact, it's never been used since it's introduced in
+    the commit 91b5c92e.
     
-    Signed-off-by: Keith Packard <keithp@keithp.com>
+    The build time option 'posix-console' _is_ in use for checking against
+    tinystdio and in newlib/libc/tinystdio/meson.build to enable building
+    posixiob.c.  The option itself is, of course, unchanged.
+    
+    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>
 
 """
 
 # Tool version info
-tool_version_str = "0.0.post108"
-tool_version_tuple = (0, 0, 108)
+tool_version_str = "0.0.post112"
+tool_version_tuple = (0, 0, 112)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post108")
+    ptool_version = V("0.0.post112")
 except ImportError:
     pass
 
