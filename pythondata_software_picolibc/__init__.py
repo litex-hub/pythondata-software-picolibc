5da86a1f9bccefe053fa70807d551f3beb0c1f6b
LiteX Robot <robot@timvideos.us>
2021-10-19 06:03:21 +0000

    Updating pythondata-software-picolibc to 0.0.post21400
    Updated data to v0.0-21288-g98160b3b9 based on 98160b3b9b8afe955765e14636de7286ff480d15 from https://github.com/picolibc/picolibc.
    > commit 98160b3b9b8afe955765e14636de7286ff480d15
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Wed Oct 13 15:20:41 2021 -0700
    >
    >     Avoid creating so many intermediate libraries
    >
    >     Instead of creating an intermediate library for every subdirectory in
    >     the tree, create lists of files that are grouped in the higher level
    >     library. This simplifies things quite a bit and should make builds run
    >     faster.
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,40 +4,37 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "0.0.post21394"
-version_tuple = (0, 0, 21394)
+version_str = "0.0.post21400"
+version_tuple = (0, 0, 21400)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post21394")
+    pversion = V("0.0.post21400")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post21282"
-data_version_tuple = (0, 0, 21282)
+data_version_str = "0.0.post21288"
+data_version_tuple = (0, 0, 21288)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post21282")
+    pdata_version = V("0.0.post21288")
 except ImportError:
     pass
-data_git_hash = "27e7610aae25917fbf54e7222a77af382558ac88"
-data_git_describe = "v0.0-21282-g27e7610aa"
+data_git_hash = "98160b3b9b8afe955765e14636de7286ff480d15"
+data_git_describe = "v0.0-21288-g98160b3b9"
 data_git_msg = """\
-commit 27e7610aae25917fbf54e7222a77af382558ac88
-Author: Yasushi SHOJI <yashi@spacecubics.com>
-Date:   Sat Oct 16 00:18:30 2021 +0900
+commit 98160b3b9b8afe955765e14636de7286ff480d15
+Author: Keith Packard <keithp@keithp.com>
+Date:   Wed Oct 13 15:20:41 2021 -0700
 
-    github/Dockerfile: Use packages.txt to have a list of packages
+    Avoid creating so many intermediate libraries
     
-    Having a list of packages in Dockerfile is something we all do.  But
-    it's not readable when we have multiple retries.  Extract the list to
-    a text file and copy it to the container before the build.  This is
-    much more readable and gives us a single list to change.
+    Instead of creating an intermediate library for every subdirectory in
+    the tree, create lists of files that are grouped in the higher level
+    library. This simplifies things quite a bit and should make builds run
+    faster.
     
-    We don't really care about additional layer created by the COPY
-    instruction because we aren't shipping the docker image.
-    
-    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>
+    Signed-off-by: Keith Packard <keithp@keithp.com>
 
 """
 
