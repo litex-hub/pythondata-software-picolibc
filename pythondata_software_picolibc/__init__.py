2f31b3a7f6795c3f125411e5884663f50c424d20
LiteX Robot <robot@timvideos.us>
2021-10-30 07:02:45 +0000

    Updating pythondata-software-picolibc to 0.0.post21454
    Updated data to v0.0-21342-g3fbd7c997 based on 3fbd7c9979b6cfd073884c874f1b7df8701e1f65 from https://github.com/picolibc/picolibc.
    > commit 3fbd7c9979b6cfd073884c874f1b7df8701e1f65
    > Author: Yasushi SHOJI <yashi@spacecubics.com>
    > Date:   Sat Oct 30 01:44:20 2021 +0900
    >
    >     meson.build: Replace explicit warning level with the core option
    >
    >     We can specify a warning level in compiler independent way with the
    >     option 'warning_level'.  This option can also be specified at the
    >     command line with -Dwarning_level=.  The default value for
    >     warning_level is 1.
    >
    >     For GCC and Clang, warning level 1 and 2 are:
    >
    >         level 1: -Wall -Winvalid-pch
    >         level 2: -Wall -Winvalid-pch -Wextra
    >
    >     So, it's the same as we used to have.
    >
    >     ref: https://mesonbuild.com/Builtin-options.html#core-options
    >
    >     Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,36 +4,46 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "0.0.post21452"
-version_tuple = (0, 0, 21452)
+version_str = "0.0.post21454"
+version_tuple = (0, 0, 21454)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post21452")
+    pversion = V("0.0.post21454")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post21340"
-data_version_tuple = (0, 0, 21340)
+data_version_str = "0.0.post21342"
+data_version_tuple = (0, 0, 21342)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post21340")
+    pdata_version = V("0.0.post21342")
 except ImportError:
     pass
-data_git_hash = "59075484682d63f4589eb49afe59e89b27e89115"
-data_git_describe = "v0.0-21340-g590754846"
+data_git_hash = "3fbd7c9979b6cfd073884c874f1b7df8701e1f65"
+data_git_describe = "v0.0-21342-g3fbd7c997"
 data_git_msg = """\
-commit 59075484682d63f4589eb49afe59e89b27e89115
-Author: Keith Packard <keithp@keithp.com>
-Date:   Thu Oct 28 11:55:30 2021 -0700
+commit 3fbd7c9979b6cfd073884c874f1b7df8701e1f65
+Author: Yasushi SHOJI <yashi@spacecubics.com>
+Date:   Sat Oct 30 01:44:20 2021 +0900
 
-    Update COPYING.picolibc
+    meson.build: Replace explicit warning level with the core option
     
-    Add musl test suite
-    Rename arm-specific files
-    Add more POSIX bits
+    We can specify a warning level in compiler independent way with the
+    option 'warning_level'.  This option can also be specified at the
+    command line with -Dwarning_level=.  The default value for
+    warning_level is 1.
     
-    Signed-off-by: Keith Packard <keithp@keithp.com>
+    For GCC and Clang, warning level 1 and 2 are:
+    
+        level 1: -Wall -Winvalid-pch
+        level 2: -Wall -Winvalid-pch -Wextra
+    
+    So, it's the same as we used to have.
+    
+    ref: https://mesonbuild.com/Builtin-options.html#core-options
+    
+    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>
 
 """
 
