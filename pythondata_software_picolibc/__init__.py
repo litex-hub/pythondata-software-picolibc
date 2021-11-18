45845664d5c9a36853ee75c1cb36743abf86aa96
LiteX Robot <robot@timvideos.us>
2021-11-18 19:02:44 +0000

    Updating pythondata-software-picolibc to 0.0.post21532
    Updated data to v0.0-21420-g1fcf6015d based on 1fcf6015d45f5f0101e8a3cb64605c04430c7428 from https://github.com/picolibc/picolibc.
    > commit 1fcf6015d45f5f0101e8a3cb64605c04430c7428
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Thu Nov 18 10:35:36 2021 -0800
    >
    >     doc: Provide command line examples for linker script usage
    >
    >     Explain how custom linker scripts can override picolibc built-in
    >     linker scripts by using the -T option on the command line.
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,34 +4,33 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "0.0.post21530"
-version_tuple = (0, 0, 21530)
+version_str = "0.0.post21532"
+version_tuple = (0, 0, 21532)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post21530")
+    pversion = V("0.0.post21532")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post21418"
-data_version_tuple = (0, 0, 21418)
+data_version_str = "0.0.post21420"
+data_version_tuple = (0, 0, 21420)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post21418")
+    pdata_version = V("0.0.post21420")
 except ImportError:
     pass
-data_git_hash = "da94a0d3a28a1d5d75ca00b2de182f4f0db391d2"
-data_git_describe = "v0.0-21418-gda94a0d3a"
+data_git_hash = "1fcf6015d45f5f0101e8a3cb64605c04430c7428"
+data_git_describe = "v0.0-21420-g1fcf6015d"
 data_git_msg = """\
-commit da94a0d3a28a1d5d75ca00b2de182f4f0db391d2
+commit 1fcf6015d45f5f0101e8a3cb64605c04430c7428
 Author: Keith Packard <keithp@keithp.com>
-Date:   Mon Nov 15 22:02:20 2021 -0800
+Date:   Thu Nov 18 10:35:36 2021 -0800
 
-    math: Use current rounding mode directly for sqrt
+    doc: Provide command line examples for linker script usage
     
-    This queries the current rounding mode rather than using computations
-    to figure out what it is. For hardware without rounding modes, all of this
-    gets short-circuited.
+    Explain how custom linker scripts can override picolibc built-in
+    linker scripts by using the -T option on the command line.
     
     Signed-off-by: Keith Packard <keithp@keithp.com>
 
