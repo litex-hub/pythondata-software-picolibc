7b9d19932280094667d87f9cd101ccfca5403386
LiteX Robot <robot@timvideos.us>
2021-11-19 06:03:37 +0000

    Updating pythondata-software-picolibc to 0.0.post21536
    Updated data to v0.0-21424-g7c2cdb9bb based on 7c2cdb9bb6810e7aaaaa49f874a60c8b7ed7e55c from https://github.com/picolibc/picolibc.
    > commit 7c2cdb9bb6810e7aaaaa49f874a60c8b7ed7e55c
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Thu Nov 18 18:00:41 2021 -0800
    >
    >     doc: Make linking.md match picolibc.ld
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post112 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,33 +4,30 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "0.0.post21534"
-version_tuple = (0, 0, 21534)
+version_str = "0.0.post21536"
+version_tuple = (0, 0, 21536)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post21534")
+    pversion = V("0.0.post21536")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post21422"
-data_version_tuple = (0, 0, 21422)
+data_version_str = "0.0.post21424"
+data_version_tuple = (0, 0, 21424)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post21422")
+    pdata_version = V("0.0.post21424")
 except ImportError:
     pass
-data_git_hash = "654f5c0a5def9fa433efd0567a9395496dfaf103"
-data_git_describe = "v0.0-21422-g654f5c0a5"
+data_git_hash = "7c2cdb9bb6810e7aaaaa49f874a60c8b7ed7e55c"
+data_git_describe = "v0.0-21424-g7c2cdb9bb"
 data_git_msg = """\
-commit 654f5c0a5def9fa433efd0567a9395496dfaf103
+commit 7c2cdb9bb6810e7aaaaa49f874a60c8b7ed7e55c
 Author: Keith Packard <keithp@keithp.com>
-Date:   Thu Nov 18 17:32:23 2021 -0800
+Date:   Thu Nov 18 18:00:41 2021 -0800
 
-    Move Documentation in README.me above release notes
-    
-    There are now a lot of release notes, which makes finding the set of
-    documentation links harder.
+    doc: Make linking.md match picolibc.ld
     
     Signed-off-by: Keith Packard <keithp@keithp.com>
 
