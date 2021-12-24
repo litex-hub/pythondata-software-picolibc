c8826e6ac19768c77c88500f5b26c6e7f6df8f45
LiteX Robot <robot@timvideos.us>
2021-12-24 00:33:00 +0000

    Updating pythondata-software-picolibc to 1.7.4.post137
    Updated data to 1.7.4-20-g243731a93 based on 243731a9303374bad23fe2caf10dc1c236af5255 from https://github.com/picolibc/picolibc.
    > commit 243731a9303374bad23fe2caf10dc1c236af5255
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Mon Dec 20 14:52:38 2021 -0800
    >
    >     test: Add tests for char/string put operations
    >
    >     Test fputc, putc, putchar, puts, fputs and fwrite
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,24 +4,24 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "0.0.post21560"
-version_tuple = (0, 0, 21560)
+version_str = "1.7.4.post137"
+version_tuple = (1, 7, 4, 137)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post21560")
+    pversion = V("1.7.4.post137")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "0.0.post21447"
-data_version_tuple = (0, 0, 21447)
+data_version_str = "1.7.4.post20"
+data_version_tuple = (1, 7, 4, 20)
 try:
     from packaging.version import Version as V
-    pdata_version = V("0.0.post21447")
+    pdata_version = V("1.7.4.post20")
 except ImportError:
     pass
 data_git_hash = "243731a9303374bad23fe2caf10dc1c236af5255"
-data_git_describe = "v0.0-21447-g243731a93"
+data_git_describe = "1.7.4-20-g243731a93"
 data_git_msg = """\
 commit 243731a9303374bad23fe2caf10dc1c236af5255
 Author: Keith Packard <keithp@keithp.com>
@@ -36,11 +36,11 @@ Date:   Mon Dec 20 14:52:38 2021 -0800
 """
 
 # Tool version info
-tool_version_str = "0.0.post113"
-tool_version_tuple = (0, 0, 113)
+tool_version_str = "0.0.post117"
+tool_version_tuple = (0, 0, 117)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post113")
+    ptool_version = V("0.0.post117")
 except ImportError:
     pass
 
