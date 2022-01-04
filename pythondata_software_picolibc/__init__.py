c6c7244303ec108c6127cb6c114d58dfef2c7f17
LiteX Robot <robot@timvideos.us>
2022-01-04 22:24:58 +0000

    Updating pythondata-software-picolibc to 1.7.4.post143
    Updated data to 1.7.4-26-g32a0dfa21 based on 32a0dfa218c13b1fa5c02465d00d42abc8d520bb from https://github.com/picolibc/picolibc.
    > commit 32a0dfa218c13b1fa5c02465d00d42abc8d520bb
    > Author: Vincent Palatin <vpalatin@rivosinc.com>
    > Date:   Mon Jan 3 18:41:01 2022 +0100
    >
    >     Fix newlib version macros
    >
    >     Newlib is defining the `__NEWLIB__` and `__NEWLIB_MINOR__` macros to export
    >     the library version.
    >     The current meson build in picolibc is exporting `_NEWLIB__` and
    >     `_NEWLIB_MINOR__` (with only one initial underscore).
    >     Align the picolibc behavior on the newlib one.
    >
    >     This fixes the build of LLVM libcxx which is using those macros to pick
    >     the right headers in the following file:
    >     https://github.com/llvm/llvm-project/blob/main/libcxx/include/__support/newlib/xlocale.h
    >
    
    Updated using 0.0.post117 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,43 +4,40 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.4.post142"
-version_tuple = (1, 7, 4, 142)
+version_str = "1.7.4.post143"
+version_tuple = (1, 7, 4, 143)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.4.post142")
+    pversion = V("1.7.4.post143")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.4.post25"
-data_version_tuple = (1, 7, 4, 25)
+data_version_str = "1.7.4.post26"
+data_version_tuple = (1, 7, 4, 26)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.4.post25")
+    pdata_version = V("1.7.4.post26")
 except ImportError:
     pass
-data_git_hash = "00a614f794c12e0fb5105a7960fbdbcc13536e50"
-data_git_describe = "1.7.4-25-g00a614f79"
+data_git_hash = "32a0dfa218c13b1fa5c02465d00d42abc8d520bb"
+data_git_describe = "1.7.4-26-g32a0dfa21"
 data_git_msg = """\
-commit 00a614f794c12e0fb5105a7960fbdbcc13536e50
-Author: Keith Packard <keithp@keithp.com>
-Date:   Mon Jan 3 13:44:45 2022 -0800
+commit 32a0dfa218c13b1fa5c02465d00d42abc8d520bb
+Author: Vincent Palatin <vpalatin@rivosinc.com>
+Date:   Mon Jan 3 18:41:01 2022 +0100
 
-    Allow cross-compile files to override has_link_defsym and has_link_alias
+    Fix newlib version macros
     
-    When building for rv32imacfdc with clang, we link with ld, which
-    requires an additional -melf32lriscv linker parameter. However, meson
-    refuses to pass that to the compiler when calling
-    compiler.has_link_argument to check for --defsym or --alias linker
-    parameters. This causes the options needed to link float-only or
-    int-only printf during testing to not be set, making the float-only
-    tests fail.
+    Newlib is defining the `__NEWLIB__` and `__NEWLIB_MINOR__` macros to export
+    the library version.
+    The current meson build in picolibc is exporting `_NEWLIB__` and
+    `_NEWLIB_MINOR__` (with only one initial underscore).
+    Align the picolibc behavior on the newlib one.
     
-    Work around this by allowing the cross-compile file to set values for
-    these two variables and skip auto-detecting them.
-    
-    Signed-off-by: Keith Packard <keithp@keithp.com>
+    This fixes the build of LLVM libcxx which is using those macros to pick
+    the right headers in the following file:
+    https://github.com/llvm/llvm-project/blob/main/libcxx/include/__support/newlib/xlocale.h
 
 """
 
