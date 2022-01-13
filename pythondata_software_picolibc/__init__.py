653864eb74985250f9c6679d83493cbac24222a7
LiteX Robot <robot@timvideos.us>
2022-01-13 18:55:53 +0000

    Updating pythondata-software-picolibc to 1.7.4.post149
    Updated data to 1.7.4-27-g29d7282a3 based on 29d7282a37fca022c8ea3214f53f0dbcc5173b63 from https://github.com/picolibc/picolibc.
    > commit 29d7282a37fca022c8ea3214f53f0dbcc5173b63
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Mon Jan 3 11:03:12 2022 -0800
    >
    >     Add double-underscore PICOLIBC version macros
    >
    >     When I initially created the meson build files, I mistakenly used only
    >     a single leading underscore in all of the version macros. This adds
    >     double leading underscore names, leaving the single underscore names
    >     for anyone using them.
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post122 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.4.post148"
-version_tuple = (1, 7, 4, 148)
+version_str = "1.7.4.post149"
+version_tuple = (1, 7, 4, 149)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.4.post148")
+    pversion = V("1.7.4.post149")
 except ImportError:
     pass
 
@@ -39,11 +39,11 @@ Date:   Mon Jan 3 11:03:12 2022 -0800
 """
 
 # Tool version info
-tool_version_str = "0.0.post121"
-tool_version_tuple = (0, 0, 121)
+tool_version_str = "0.0.post122"
+tool_version_tuple = (0, 0, 122)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post121")
+    ptool_version = V("0.0.post122")
 except ImportError:
     pass
 
