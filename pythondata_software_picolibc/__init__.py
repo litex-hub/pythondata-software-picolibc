a6279829e1d615d47618a31b492dc5970094f5fb
LiteX Robot <robot@timvideos.us>
2022-02-11 01:26:45 +0000

    Updating pythondata-software-picolibc to 1.7.4.post358
    Updated data to 1.7.4-234-gd79f1d606 based on d79f1d606a71147ef8c1ed36594b5ed6a2e96122 from https://github.com/picolibc/picolibc.
    > commit d79f1d606a71147ef8c1ed36594b5ed6a2e96122
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Thu Feb 10 15:53:40 2022 -0800
    >
    >     Update build status link to use new 'Linux' workflow
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post124 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,30 +4,30 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.4.post356"
-version_tuple = (1, 7, 4, 356)
+version_str = "1.7.4.post358"
+version_tuple = (1, 7, 4, 358)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.4.post356")
+    pversion = V("1.7.4.post358")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.4.post232"
-data_version_tuple = (1, 7, 4, 232)
+data_version_str = "1.7.4.post234"
+data_version_tuple = (1, 7, 4, 234)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.4.post232")
+    pdata_version = V("1.7.4.post234")
 except ImportError:
     pass
-data_git_hash = "f80f774c3f263c75219d2e90820043369b0b0b34"
-data_git_describe = "1.7.4-232-gf80f774c3"
+data_git_hash = "d79f1d606a71147ef8c1ed36594b5ed6a2e96122"
+data_git_describe = "1.7.4-234-gd79f1d606"
 data_git_msg = """\
-commit f80f774c3f263c75219d2e90820043369b0b0b34
+commit d79f1d606a71147ef8c1ed36594b5ed6a2e96122
 Author: Keith Packard <keithp@keithp.com>
-Date:   Sun Feb 6 20:09:04 2022 -0800
+Date:   Thu Feb 10 15:53:40 2022 -0800
 
-    .github: Add build tests for PowerPC
+    Update build status link to use new 'Linux' workflow
     
     Signed-off-by: Keith Packard <keithp@keithp.com>
 
