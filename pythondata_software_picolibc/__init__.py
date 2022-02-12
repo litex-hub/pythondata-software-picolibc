0d44d909b445a12601598e012f57b778f339343f
LiteX Robot <robot@timvideos.us>
2022-02-12 19:08:44 +0000

    Updating pythondata-software-picolibc to 1.7.4.post360
    Updated data to 1.7.4-234-gd79f1d606 based on d79f1d606a71147ef8c1ed36594b5ed6a2e96122 from https://github.com/picolibc/picolibc.
    > commit d79f1d606a71147ef8c1ed36594b5ed6a2e96122
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Thu Feb 10 15:53:40 2022 -0800
    >
    >     Update build status link to use new 'Linux' workflow
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.4.post358"
-version_tuple = (1, 7, 4, 358)
+version_str = "1.7.4.post360"
+version_tuple = (1, 7, 4, 360)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.4.post358")
+    pversion = V("1.7.4.post360")
 except ImportError:
     pass
 
@@ -34,11 +34,11 @@ Date:   Thu Feb 10 15:53:40 2022 -0800
 """
 
 # Tool version info
-tool_version_str = "0.0.post124"
-tool_version_tuple = (0, 0, 124)
+tool_version_str = "0.0.post126"
+tool_version_tuple = (0, 0, 126)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post124")
+    ptool_version = V("0.0.post126")
 except ImportError:
     pass
 
