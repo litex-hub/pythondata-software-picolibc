97da2b6e168386cdfa6d5c8bf5c834d17cc1edab
LiteX Robot <robot@timvideos.us>
2022-02-19 22:29:24 +0000

    Updating pythondata-software-picolibc to 1.7.4.post497
    Updated data to 1.7.4-371-gd980f357f based on d980f357f4958c4caf4facaca3317fc81ebc6ecb from https://github.com/picolibc/picolibc.
    > commit d980f357f4958c4caf4facaca3317fc81ebc6ecb
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Fri Feb 18 21:57:50 2022 -0800
    >
    >     scripts: Switch m68k sample to use do-configure script
    >
    >     There's no reason to have a custom script for this target. At the same
    >     time, switch from building 68000 code to 68020 as that seems easier to
    >     link.
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,33 +4,34 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.4.post493"
-version_tuple = (1, 7, 4, 493)
+version_str = "1.7.4.post497"
+version_tuple = (1, 7, 4, 497)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.4.post493")
+    pversion = V("1.7.4.post497")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.4.post367"
-data_version_tuple = (1, 7, 4, 367)
+data_version_str = "1.7.4.post371"
+data_version_tuple = (1, 7, 4, 371)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.4.post367")
+    pdata_version = V("1.7.4.post371")
 except ImportError:
     pass
-data_git_hash = "cea19312063fbe42d1388179d09e1102feb85918"
-data_git_describe = "1.7.4-367-gcea193120"
+data_git_hash = "d980f357f4958c4caf4facaca3317fc81ebc6ecb"
+data_git_describe = "1.7.4-371-gd980f357f"
 data_git_msg = """\
-commit cea19312063fbe42d1388179d09e1102feb85918
+commit d980f357f4958c4caf4facaca3317fc81ebc6ecb
 Author: Keith Packard <keithp@keithp.com>
-Date:   Sun Feb 13 23:28:20 2022 -0800
+Date:   Fri Feb 18 21:57:50 2022 -0800
 
-    test: Ignore -Wstringop-truncation when building printf_scanf
+    scripts: Switch m68k sample to use do-configure script
     
-    This test triggers a warning when using strncpy in valid, if slightly
-    unusual way.
+    There's no reason to have a custom script for this target. At the same
+    time, switch from building 68000 code to 68020 as that seems easier to
+    link.
     
     Signed-off-by: Keith Packard <keithp@keithp.com>
 
