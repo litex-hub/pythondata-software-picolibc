fa961d06cf829b11a6147fd6852c56fd47f5927b
LiteX Robot <robot@timvideos.us>
2022-03-02 06:35:01 +0000

    Updating pythondata-software-picolibc to 1.7.4.post499
    Updated data to 1.7.4-373-g8359908e5 based on 8359908e586e838cc1562df36e784fce489c058d from https://github.com/picolibc/picolibc.
    > commit 8359908e586e838cc1562df36e784fce489c058d
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Tue Mar 1 17:32:24 2022 -0800
    >
    >     Add '-Dbuild-type-subdir' to meson and '--picolibc-buildtype' to gcc
    >
    >     These two options let picolibc be built with multiple configuration options and
    >     installed in the same target directory. At application compile time, the build type
    >     can be selected via the GCC --picolibc-buildtype option which selects which target
    >     path to use for headers and binaries.
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,34 +4,35 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.4.post497"
-version_tuple = (1, 7, 4, 497)
+version_str = "1.7.4.post499"
+version_tuple = (1, 7, 4, 499)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.4.post497")
+    pversion = V("1.7.4.post499")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.4.post371"
-data_version_tuple = (1, 7, 4, 371)
+data_version_str = "1.7.4.post373"
+data_version_tuple = (1, 7, 4, 373)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.4.post371")
+    pdata_version = V("1.7.4.post373")
 except ImportError:
     pass
-data_git_hash = "d980f357f4958c4caf4facaca3317fc81ebc6ecb"
-data_git_describe = "1.7.4-371-gd980f357f"
+data_git_hash = "8359908e586e838cc1562df36e784fce489c058d"
+data_git_describe = "1.7.4-373-g8359908e5"
 data_git_msg = """\
-commit d980f357f4958c4caf4facaca3317fc81ebc6ecb
+commit 8359908e586e838cc1562df36e784fce489c058d
 Author: Keith Packard <keithp@keithp.com>
-Date:   Fri Feb 18 21:57:50 2022 -0800
+Date:   Tue Mar 1 17:32:24 2022 -0800
 
-    scripts: Switch m68k sample to use do-configure script
+    Add '-Dbuild-type-subdir' to meson and '--picolibc-buildtype' to gcc
     
-    There's no reason to have a custom script for this target. At the same
-    time, switch from building 68000 code to 68020 as that seems easier to
-    link.
+    These two options let picolibc be built with multiple configuration options and
+    installed in the same target directory. At application compile time, the build type
+    can be selected via the GCC --picolibc-buildtype option which selects which target
+    path to use for headers and binaries.
     
     Signed-off-by: Keith Packard <keithp@keithp.com>
 
