88dd49e4fd163c490bff51695b48c46799bee93f
LiteX Robot <robot@timvideos.us>
2022-03-29 01:58:38 +0000

    Updating pythondata-software-picolibc to 1.7.6.post130
    Updated data to 1.7.6-4-gb3a735784 based on b3a735784c3f9fe45721f7caea88fccee16ea922 from https://github.com/picolibc/picolibc.
    > commit b3a735784c3f9fe45721f7caea88fccee16ea922
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Mon Mar 28 11:32:16 2022 -0700
    >
    >     test/semihost: Add test for gettimeofday
    >
    >     Make sure this function returns reasonable values:
    >
    >      * After september 2020 (unix time 1,600,000,000)
    >      * Values never decrease
    >      * New values are within 1000 seconds of original value
    >      * Eventually changes after no more than 1000 iterations
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,30 +4,37 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.6.post126"
-version_tuple = (1, 7, 6, 126)
+version_str = "1.7.6.post130"
+version_tuple = (1, 7, 6, 130)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.6.post126")
+    pversion = V("1.7.6.post130")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.6.post0"
-data_version_tuple = (1, 7, 6, 0)
+data_version_str = "1.7.6.post4"
+data_version_tuple = (1, 7, 6, 4)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.6.post0")
+    pdata_version = V("1.7.6.post4")
 except ImportError:
     pass
-data_git_hash = "19d368939508d228439c996a0dcef7ff99be4d12"
-data_git_describe = "1.7.6-0-g19d368939"
+data_git_hash = "b3a735784c3f9fe45721f7caea88fccee16ea922"
+data_git_describe = "1.7.6-4-gb3a735784"
 data_git_msg = """\
-commit 19d368939508d228439c996a0dcef7ff99be4d12
+commit b3a735784c3f9fe45721f7caea88fccee16ea922
 Author: Keith Packard <keithp@keithp.com>
-Date:   Wed Mar 16 12:58:25 2022 -0700
+Date:   Mon Mar 28 11:32:16 2022 -0700
 
-    Version 1.7.6
+    test/semihost: Add test for gettimeofday
+    
+    Make sure this function returns reasonable values:
+    
+     * After september 2020 (unix time 1,600,000,000)
+     * Values never decrease
+     * New values are within 1000 seconds of original value
+     * Eventually changes after no more than 1000 iterations
     
     Signed-off-by: Keith Packard <keithp@keithp.com>
 
