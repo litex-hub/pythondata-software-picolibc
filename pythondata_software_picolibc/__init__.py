0efae647879060104d1bae81d526a7cf0e1993db
LiteX Robot <robot@timvideos.us>
2022-04-13 17:32:53 +0000

    Updating pythondata-software-picolibc to 1.7.6.post142
    Updated data to 1.7.6-16-gaf27334b3 based on af27334b30bf9192a1984f4f1e6a661f18867e4f from https://github.com/picolibc/picolibc.
    > commit af27334b30bf9192a1984f4f1e6a661f18867e4f
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Tue Apr 12 09:40:58 2022 -0700
    >
    >     tinystdio: Ignore thousands separator flag
    >
    >     POSIX defines a thousands separator flag for printf, ', which causes
    >     the current locale thousands separator to be placed between groups of
    >     digits to make them easier to read. As tinystdio doesn't have any
    >     locale support, and as the C/POSIX locale has an empty thousands
    >     separator, we can "support" this feature by simply ignoring this flag.
    >
    >     Enabling this feature in a useful fashion will require either some
    >     level of locale support or creating a custom API to control it.
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post126 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,31 +4,41 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.6.post141"
-version_tuple = (1, 7, 6, 141)
+version_str = "1.7.6.post142"
+version_tuple = (1, 7, 6, 142)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.6.post141")
+    pversion = V("1.7.6.post142")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.6.post15"
-data_version_tuple = (1, 7, 6, 15)
+data_version_str = "1.7.6.post16"
+data_version_tuple = (1, 7, 6, 16)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.6.post15")
+    pdata_version = V("1.7.6.post16")
 except ImportError:
     pass
-data_git_hash = "4068b85b9b48566caee79d66b1f9bac79f728a8c"
-data_git_describe = "1.7.6-15-g4068b85b9"
+data_git_hash = "af27334b30bf9192a1984f4f1e6a661f18867e4f"
+data_git_describe = "1.7.6-16-gaf27334b3"
 data_git_msg = """\
-commit 4068b85b9b48566caee79d66b1f9bac79f728a8c
-Merge: 2ffcc0768 2efd2d18f
+commit af27334b30bf9192a1984f4f1e6a661f18867e4f
 Author: Keith Packard <keithp@keithp.com>
-Date:   Tue Apr 12 10:10:53 2022 -0700
+Date:   Tue Apr 12 09:40:58 2022 -0700
 
-    Merge remote-tracking branch 'aykevl/fix-16-bit-int-shift'
+    tinystdio: Ignore thousands separator flag
+    
+    POSIX defines a thousands separator flag for printf, ', which causes
+    the current locale thousands separator to be placed between groups of
+    digits to make them easier to read. As tinystdio doesn't have any
+    locale support, and as the C/POSIX locale has an empty thousands
+    separator, we can "support" this feature by simply ignoring this flag.
+    
+    Enabling this feature in a useful fashion will require either some
+    level of locale support or creating a custom API to control it.
+    
+    Signed-off-by: Keith Packard <keithp@keithp.com>
 
 """
 
