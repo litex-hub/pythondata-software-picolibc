4b2867ca1455ee3f862862b594fc649cefee902d
LiteX Robot <robot@timvideos.us>
2022-06-23 00:40:27 +0000

    Updating pythondata-software-picolibc to 1.7.7.post156
    Updated data to 1.7.7-14-ge87b2fc37 based on e87b2fc37345a62361478f0a6efd140e14180ba5 from https://github.com/picolibc/picolibc.
    > commit e87b2fc37345a62361478f0a6efd140e14180ba5
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Wed Jun 22 11:08:35 2022 -0700
    >
    >     cmake: Allow setting of __PICOLIBC_ERRNO_FUNCTION
    >
    >     This is needed for non-TLS builds of Zephyr.
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,33 +4,32 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.7.post155"
-version_tuple = (1, 7, 7, 155)
+version_str = "1.7.7.post156"
+version_tuple = (1, 7, 7, 156)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.7.post155")
+    pversion = V("1.7.7.post156")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.7.post13"
-data_version_tuple = (1, 7, 7, 13)
+data_version_str = "1.7.7.post14"
+data_version_tuple = (1, 7, 7, 14)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.7.post13")
+    pdata_version = V("1.7.7.post14")
 except ImportError:
     pass
-data_git_hash = "bbcbb51c17bc9afbae9e1683ac4c7b3e740bf2c8"
-data_git_describe = "1.7.7-13-gbbcbb51c1"
+data_git_hash = "e87b2fc37345a62361478f0a6efd140e14180ba5"
+data_git_describe = "1.7.7-14-ge87b2fc37"
 data_git_msg = """\
-commit bbcbb51c17bc9afbae9e1683ac4c7b3e740bf2c8
+commit e87b2fc37345a62361478f0a6efd140e14180ba5
 Author: Keith Packard <keithp@keithp.com>
-Date:   Tue Jun 21 14:09:49 2022 -0700
+Date:   Wed Jun 22 11:08:35 2022 -0700
 
-    test: Add test for malloc(PTRDIFF_MAX)
+    cmake: Allow setting of __PICOLIBC_ERRNO_FUNCTION
     
-    This can have a different failure mode than malloc(SIZE_MAX), so test
-    it explicitly
+    This is needed for non-TLS builds of Zephyr.
     
     Signed-off-by: Keith Packard <keithp@keithp.com>
 
