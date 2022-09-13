29899ab4883f8186539c95be161d65da1a0a8c8f
LiteX Robot <robot@timvideos.us>
2022-09-13 09:17:27 +0000

    Updating pythondata-software-picolibc to 1.7.8.post449
    Updated data to 1.7.8-307-g95b1921f9 based on 95b1921f928ee5e373419dcf6a9706e1e6049228 from https://github.com/picolibc/picolibc.
    > commit 95b1921f928ee5e373419dcf6a9706e1e6049228
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Mon Sep 12 16:21:42 2022 +0100
    >
    >     reent: Eliminate reent.h and sys/reent.h
    >
    >     Clean up the last few files using these headers, then
    >     remove them entirely.
    >
    >     This removes a bunch of unused powerpc code as that
    >     also used reent, but gcc no longer supports the SPE option
    >     needed to use any of it.
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,33 +4,37 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.8.post173"
-version_tuple = (1, 7, 8, 173)
+version_str = "1.7.8.post449"
+version_tuple = (1, 7, 8, 449)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.8.post173")
+    pversion = V("1.7.8.post449")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.8.post31"
-data_version_tuple = (1, 7, 8, 31)
+data_version_str = "1.7.8.post307"
+data_version_tuple = (1, 7, 8, 307)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.8.post31")
+    pdata_version = V("1.7.8.post307")
 except ImportError:
     pass
-data_git_hash = "569e2895a300237c5aae75c78c79daa74b09a11a"
-data_git_describe = "1.7.8-31-g569e2895a"
+data_git_hash = "95b1921f928ee5e373419dcf6a9706e1e6049228"
+data_git_describe = "1.7.8-307-g95b1921f9"
 data_git_msg = """\
-commit 569e2895a300237c5aae75c78c79daa74b09a11a
+commit 95b1921f928ee5e373419dcf6a9706e1e6049228
 Author: Keith Packard <keithp@keithp.com>
-Date:   Fri Aug 26 16:39:18 2022 -0700
+Date:   Mon Sep 12 16:21:42 2022 +0100
 
-    .github: Test non-picoexit configuration
+    reent: Eliminate reent.h and sys/reent.h
     
-    Looks like there's a bug here, so let's test it and make sure it fails before
-    adding a fix.
+    Clean up the last few files using these headers, then
+    remove them entirely.
+    
+    This removes a bunch of unused powerpc code as that
+    also used reent, but gcc no longer supports the SPE option
+    needed to use any of it.
     
     Signed-off-by: Keith Packard <keithp@keithp.com>
 
