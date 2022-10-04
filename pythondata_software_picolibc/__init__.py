274e3e55053d003ae0bf6df594940c91367d3573
LiteX Robot <robot@timvideos.us>
2022-10-04 05:44:19 +0000

    Updating pythondata-software-picolibc to 1.7.8.post477
    Updated data to 1.7.8-335-g422275831 based on 4222758310860d2c65193429655943166c6be400 from https://github.com/picolibc/picolibc.
    > commit 4222758310860d2c65193429655943166c6be400
    > Author: Michael Platings <michael.platings@arm.com>
    > Date:   Mon Oct 3 18:45:41 2022 +0100
    >
    >     Add mbstate_t.h for compatibility with libc++
    >
    >     tinystdio doesn't provide wide character functions like wcstold.
    >     Therefore libc++ must be built with wide character support disabled.
    >     When libc++ wide character support is disabled, it can't include
    >     wchar.h. However wchar.h is where mbstate_t is defined.
    >     Several types like std::fpos are defined in terms of mbstate_t so it's
    >     needed even if wide character support isn't. To get around this, libc++
    >     attempts to include the non-standard header mbstate_t.h. This change
    >     adds mbstate_t.h in the location that libc++ expects to find it.
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,34 +4,39 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.8.post473"
-version_tuple = (1, 7, 8, 473)
+version_str = "1.7.8.post477"
+version_tuple = (1, 7, 8, 477)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.8.post473")
+    pversion = V("1.7.8.post477")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.8.post331"
-data_version_tuple = (1, 7, 8, 331)
+data_version_str = "1.7.8.post335"
+data_version_tuple = (1, 7, 8, 335)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.8.post331")
+    pdata_version = V("1.7.8.post335")
 except ImportError:
     pass
-data_git_hash = "5c5f2a2e2a46066fafae38d228e1550339578fd0"
-data_git_describe = "1.7.8-331-g5c5f2a2e2"
+data_git_hash = "4222758310860d2c65193429655943166c6be400"
+data_git_describe = "1.7.8-335-g422275831"
 data_git_msg = """\
-commit 5c5f2a2e2a46066fafae38d228e1550339578fd0
-Author: Keith Packard <keithp@keithp.com>
-Date:   Wed Sep 14 15:35:19 2022 +0100
+commit 4222758310860d2c65193429655943166c6be400
+Author: Michael Platings <michael.platings@arm.com>
+Date:   Mon Oct 3 18:45:41 2022 +0100
 
-    .github: Build MSP430 bits
+    Add mbstate_t.h for compatibility with libc++
     
-    Use the TI MSP430 GNU SDK to build using both clang and gcc.
-    
-    Signed-off-by: Keith Packard <keithp@keithp.com>
+    tinystdio doesn't provide wide character functions like wcstold.
+    Therefore libc++ must be built with wide character support disabled.
+    When libc++ wide character support is disabled, it can't include
+    wchar.h. However wchar.h is where mbstate_t is defined.
+    Several types like std::fpos are defined in terms of mbstate_t so it's
+    needed even if wide character support isn't. To get around this, libc++
+    attempts to include the non-standard header mbstate_t.h. This change
+    adds mbstate_t.h in the location that libc++ expects to find it.
 
 """
 
