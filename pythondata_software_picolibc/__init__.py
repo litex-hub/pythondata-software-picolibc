6a4b7ba61e9015971465c5b6372d9f4c3a23030f
LiteX Robot <robot@timvideos.us>
2022-10-05 04:43:13 +0000

    Updating pythondata-software-picolibc to 1.7.8.post480
    Updated data to 1.7.8-338-g7beed395a based on 7beed395a06b5a977dfa4cdcfc13ee4fe8685a71 from https://github.com/picolibc/picolibc.
    > commit 7beed395a06b5a977dfa4cdcfc13ee4fe8685a71
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Tue Oct 4 17:41:43 2022 -0700
    >
    >     .github: Test x86_64 and multi-lib x86 builds
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,39 +4,32 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.8.post477"
-version_tuple = (1, 7, 8, 477)
+version_str = "1.7.8.post480"
+version_tuple = (1, 7, 8, 480)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.8.post477")
+    pversion = V("1.7.8.post480")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.8.post335"
-data_version_tuple = (1, 7, 8, 335)
+data_version_str = "1.7.8.post338"
+data_version_tuple = (1, 7, 8, 338)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.8.post335")
+    pdata_version = V("1.7.8.post338")
 except ImportError:
     pass
-data_git_hash = "4222758310860d2c65193429655943166c6be400"
-data_git_describe = "1.7.8-335-g422275831"
+data_git_hash = "7beed395a06b5a977dfa4cdcfc13ee4fe8685a71"
+data_git_describe = "1.7.8-338-g7beed395a"
 data_git_msg = """\
-commit 4222758310860d2c65193429655943166c6be400
-Author: Michael Platings <michael.platings@arm.com>
-Date:   Mon Oct 3 18:45:41 2022 +0100
+commit 7beed395a06b5a977dfa4cdcfc13ee4fe8685a71
+Author: Keith Packard <keithp@keithp.com>
+Date:   Tue Oct 4 17:41:43 2022 -0700
 
-    Add mbstate_t.h for compatibility with libc++
+    .github: Test x86_64 and multi-lib x86 builds
     
-    tinystdio doesn't provide wide character functions like wcstold.
-    Therefore libc++ must be built with wide character support disabled.
-    When libc++ wide character support is disabled, it can't include
-    wchar.h. However wchar.h is where mbstate_t is defined.
-    Several types like std::fpos are defined in terms of mbstate_t so it's
-    needed even if wide character support isn't. To get around this, libc++
-    attempts to include the non-standard header mbstate_t.h. This change
-    adds mbstate_t.h in the location that libc++ expects to find it.
+    Signed-off-by: Keith Packard <keithp@keithp.com>
 
 """
 
