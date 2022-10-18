fe2199931d502cb614313745ed0163f61eea569e
LiteX Robot <robot@timvideos.us>
2022-10-18 01:04:48 +0000

    Updating pythondata-software-picolibc to 1.7.9.post149
    Updated data to 1.7.9-7-g975d41b7f based on 975d41b7fdf044ba917fa836f77df786c205d4e3 from https://github.com/picolibc/picolibc.
    > commit 975d41b7fdf044ba917fa836f77df786c205d4e3
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Mon Oct 17 13:51:00 2022 -0700
    >
    >     picolibc/arc: Add _set_tls support for ARC targets
    >
    >     Use the __ARC_GLS_REGNO__ define for this code. This requires
    >     that the configuration specify -mtp-regno= correctly.
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,34 +4,33 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.9.post146"
-version_tuple = (1, 7, 9, 146)
+version_str = "1.7.9.post149"
+version_tuple = (1, 7, 9, 149)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.9.post146")
+    pversion = V("1.7.9.post149")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.9.post4"
-data_version_tuple = (1, 7, 9, 4)
+data_version_str = "1.7.9.post7"
+data_version_tuple = (1, 7, 9, 7)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.9.post4")
+    pdata_version = V("1.7.9.post7")
 except ImportError:
     pass
-data_git_hash = "481c4b5ac4781d2fbba8c1cf66fe7da48d4a831b"
-data_git_describe = "1.7.9-4-g481c4b5ac"
+data_git_hash = "975d41b7fdf044ba917fa836f77df786c205d4e3"
+data_git_describe = "1.7.9-7-g975d41b7f"
 data_git_msg = """\
-commit 481c4b5ac4781d2fbba8c1cf66fe7da48d4a831b
+commit 975d41b7fdf044ba917fa836f77df786c205d4e3
 Author: Keith Packard <keithp@keithp.com>
-Date:   Thu Oct 13 23:17:49 2022 -0700
+Date:   Mon Oct 17 13:51:00 2022 -0700
 
-    cmake: Skip asm code on arcv3
+    picolibc/arc: Add _set_tls support for ARC targets
     
-    The accelerated asm code only works on older arc targets, just use the
-    C code versions on new hardware. This already worked with meson, which
-    checks a CPP symbol. The cmake bits use CONFIG values instead.
+    Use the __ARC_GLS_REGNO__ define for this code. This requires
+    that the configuration specify -mtp-regno= correctly.
     
     Signed-off-by: Keith Packard <keithp@keithp.com>
 
