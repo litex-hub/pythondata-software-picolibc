4e297260df0951f248e7f3a5f9959e0a99897cee
LiteX Robot <robot@timvideos.us>
2022-10-19 21:19:51 +0000

    Updating pythondata-software-picolibc to 1.7.9.post151
    Updated data to 1.7.9-9-g28db410e5 based on 28db410e56cc9eeed0f6ef493aea26755ef644e1 from https://github.com/picolibc/picolibc.
    > commit 28db410e56cc9eeed0f6ef493aea26755ef644e1
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Wed Oct 19 08:58:44 2022 -0700
    >
    >     .github: Build arc configurations in CI
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,33 +4,30 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.9.post149"
-version_tuple = (1, 7, 9, 149)
+version_str = "1.7.9.post151"
+version_tuple = (1, 7, 9, 151)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.9.post149")
+    pversion = V("1.7.9.post151")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.9.post7"
-data_version_tuple = (1, 7, 9, 7)
+data_version_str = "1.7.9.post9"
+data_version_tuple = (1, 7, 9, 9)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.9.post7")
+    pdata_version = V("1.7.9.post9")
 except ImportError:
     pass
-data_git_hash = "975d41b7fdf044ba917fa836f77df786c205d4e3"
-data_git_describe = "1.7.9-7-g975d41b7f"
+data_git_hash = "28db410e56cc9eeed0f6ef493aea26755ef644e1"
+data_git_describe = "1.7.9-9-g28db410e5"
 data_git_msg = """\
-commit 975d41b7fdf044ba917fa836f77df786c205d4e3
+commit 28db410e56cc9eeed0f6ef493aea26755ef644e1
 Author: Keith Packard <keithp@keithp.com>
-Date:   Mon Oct 17 13:51:00 2022 -0700
+Date:   Wed Oct 19 08:58:44 2022 -0700
 
-    picolibc/arc: Add _set_tls support for ARC targets
-    
-    Use the __ARC_GLS_REGNO__ define for this code. This requires
-    that the configuration specify -mtp-regno= correctly.
+    .github: Build arc configurations in CI
     
     Signed-off-by: Keith Packard <keithp@keithp.com>
 
