a3dff6a6fd3a7f8503f90e8aafd41905a797bdfc
LiteX Robot <robot@timvideos.us>
2022-10-20 05:01:03 +0000

    Updating pythondata-software-picolibc to 1.7.9.post152
    Updated data to 1.7.9-10-g8a98d4d6b based on 8a98d4d6b5c9ae43d6a980bfd5678fe88fbdf35c from https://github.com/picolibc/picolibc.
    > commit 8a98d4d6b5c9ae43d6a980bfd5678fe88fbdf35c
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Wed Oct 19 10:33:39 2022 -0700
    >
    >     xtensa: Add _set_tls helper
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,30 +4,30 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.9.post151"
-version_tuple = (1, 7, 9, 151)
+version_str = "1.7.9.post152"
+version_tuple = (1, 7, 9, 152)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.9.post151")
+    pversion = V("1.7.9.post152")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.9.post9"
-data_version_tuple = (1, 7, 9, 9)
+data_version_str = "1.7.9.post10"
+data_version_tuple = (1, 7, 9, 10)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.9.post9")
+    pdata_version = V("1.7.9.post10")
 except ImportError:
     pass
-data_git_hash = "28db410e56cc9eeed0f6ef493aea26755ef644e1"
-data_git_describe = "1.7.9-9-g28db410e5"
+data_git_hash = "8a98d4d6b5c9ae43d6a980bfd5678fe88fbdf35c"
+data_git_describe = "1.7.9-10-g8a98d4d6b"
 data_git_msg = """\
-commit 28db410e56cc9eeed0f6ef493aea26755ef644e1
+commit 8a98d4d6b5c9ae43d6a980bfd5678fe88fbdf35c
 Author: Keith Packard <keithp@keithp.com>
-Date:   Wed Oct 19 08:58:44 2022 -0700
+Date:   Wed Oct 19 10:33:39 2022 -0700
 
-    .github: Build arc configurations in CI
+    xtensa: Add _set_tls helper
     
     Signed-off-by: Keith Packard <keithp@keithp.com>
 
