269504d370d981d2faff1c556511b34958e16479
LiteX Robot <robot@timvideos.us>
2022-10-21 04:24:53 +0000

    Updating pythondata-software-picolibc to 1.7.9.post155
    Updated data to 1.7.9-13-ge4714f3be based on e4714f3bef99de28bd17d3e43c1cd2bb6dc41994 from https://github.com/picolibc/picolibc.
    > commit e4714f3bef99de28bd17d3e43c1cd2bb6dc41994
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Thu Oct 20 10:52:16 2022 -0700
    >
    >     libc/stdlib: Implement locale-free versions of strto* functions
    >
    >     Use code from tinystdio scanf to build small versions of these
    >     functions that don't depend on the locale code for 'isspace'.
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,33 +4,35 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.9.post153"
-version_tuple = (1, 7, 9, 153)
+version_str = "1.7.9.post155"
+version_tuple = (1, 7, 9, 155)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.9.post153")
+    pversion = V("1.7.9.post155")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.9.post11"
-data_version_tuple = (1, 7, 9, 11)
+data_version_str = "1.7.9.post13"
+data_version_tuple = (1, 7, 9, 13)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.9.post11")
+    pdata_version = V("1.7.9.post13")
 except ImportError:
     pass
-data_git_hash = "67574211585540c048ccc5df5f46a37055e2eb5d"
-data_git_describe = "1.7.9-11-g675742115"
+data_git_hash = "e4714f3bef99de28bd17d3e43c1cd2bb6dc41994"
+data_git_describe = "1.7.9-13-ge4714f3be"
 data_git_msg = """\
-commit 67574211585540c048ccc5df5f46a37055e2eb5d
-Author: Troels Jessen <troels@space-inventor.com>
-Date:   Fri Sep 30 09:48:00 2022 +0200
+commit e4714f3bef99de28bd17d3e43c1cd2bb6dc41994
+Author: Keith Packard <keithp@keithp.com>
+Date:   Thu Oct 20 10:52:16 2022 -0700
 
-    Remove double underscores in defines
+    libc/stdlib: Implement locale-free versions of strto* functions
     
-    Defines with double underscores does not exist for all compilers,
-    causing environment to default to big endian
+    Use code from tinystdio scanf to build small versions of these
+    functions that don't depend on the locale code for 'isspace'.
+    
+    Signed-off-by: Keith Packard <keithp@keithp.com>
 
 """
 
