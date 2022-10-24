9083c64eb9be98b9f6a257be4b22ade569c2a694
LiteX Robot <robot@timvideos.us>
2022-10-24 01:12:22 +0000

    Updating pythondata-software-picolibc to 1.7.9.post164
    Updated data to 1.7.9-22-ge2d558295 based on e2d558295bed357cb6ec47a545533afb9d7151fe from https://github.com/picolibc/picolibc.
    > commit e2d558295bed357cb6ec47a545533afb9d7151fe
    > Author: Keith Packard <keithp@keithp.com>
    > Date:   Sun Oct 23 14:13:04 2022 -0700
    >
    >     test: Exhaustively test strtol in-range/out-of-range parsing
    >
    >     Make sure strtol deals with every possible character value correctly,
    >     assigning it to be either in-range or out-of-range.
    >
    >     Signed-off-by: Keith Packard <keithp@keithp.com>
    >
    
    Updated using 0.0.post142 from https://github.com/litex-hub/litex-data-auto

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -4,36 +4,33 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/picolibc/picolibc"
 
 # Module version
-version_str = "1.7.9.post162"
-version_tuple = (1, 7, 9, 162)
+version_str = "1.7.9.post164"
+version_tuple = (1, 7, 9, 164)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.9.post162")
+    pversion = V("1.7.9.post164")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.9.post20"
-data_version_tuple = (1, 7, 9, 20)
+data_version_str = "1.7.9.post22"
+data_version_tuple = (1, 7, 9, 22)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.9.post20")
+    pdata_version = V("1.7.9.post22")
 except ImportError:
     pass
-data_git_hash = "c2eaa1717f0681de12dc8012bd261dd0ddcf0ade"
-data_git_describe = "1.7.9-20-gc2eaa1717"
+data_git_hash = "e2d558295bed357cb6ec47a545533afb9d7151fe"
+data_git_describe = "1.7.9-22-ge2d558295"
 data_git_msg = """\
-commit c2eaa1717f0681de12dc8012bd261dd0ddcf0ade
+commit e2d558295bed357cb6ec47a545533afb9d7151fe
 Author: Keith Packard <keithp@keithp.com>
-Date:   Sat Oct 22 13:15:43 2022 -0700
+Date:   Sun Oct 23 14:13:04 2022 -0700
 
-    test: Add 64-bit strtol tests as well and ':' termination test
+    test: Exhaustively test strtol in-range/out-of-range parsing
     
-    Strtol was mis-parsing ipv6 addrs as the separating ':' was not
-    correctly detected as the end of a hex number. Add a test for this
-    case.
-    
-    Also replicate the overflow tests for 64-bit longs.
+    Make sure strtol deals with every possible character value correctly,
+    assigning it to be either in-range or out-of-range.
     
     Signed-off-by: Keith Packard <keithp@keithp.com>
 
