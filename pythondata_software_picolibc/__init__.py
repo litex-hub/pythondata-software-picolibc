6a13ccce7c575b32c102dd9dc52178505b81fe39
Florent Kermarrec <florent@enjoy-digital.fr>
2026-06-05 18:24:13 +0200

    data: Make PPC alias commit fetchable
    The current data submodule points at picolibc commit 16ff442da4b9, which adds the missing ppc -> powerpc Meson CPU family alias needed by LiteX PPC builds.
    
    That commit was not reachable from the upstream picolibc/picolibc remote, so recursive clones failed in CI with upload-pack: not our ref. Publish the commit on the LiteX-Hub picolibc fork and update the wrapper submodule URL/branch to use that public fork.
    
    Refresh the package metadata so it reports the actual 1.8.11.post1 data commit.

--- pythondata_software_picolibc/__init__.py
+++ pythondata_software_picolibc/__init__.py
@@ -1,39 +1,37 @@
 import os.path
 __dir__ = os.path.split(os.path.abspath(os.path.realpath(__file__)))[0]
 data_location = os.path.join(__dir__, "data")
-src = "https://github.com/picolibc/picolibc"
+src = "https://github.com/litex-hub/picolibc"
 
 # Module version
-version_str = "1.7.9.post181"
-version_tuple = (1, 7, 9, 181)
+version_str = "1.8.11.post1"
+version_tuple = (1, 8, 11, 1)
 try:
     from packaging.version import Version as V
-    pversion = V("1.7.9.post181")
+    pversion = V("1.8.11.post1")
 except ImportError:
     pass
 
 # Data version info
-data_version_str = "1.7.9.post39"
-data_version_tuple = (1, 7, 9, 39)
+data_version_str = "1.8.11.post1"
+data_version_tuple = (1, 8, 11, 1)
 try:
     from packaging.version import Version as V
-    pdata_version = V("1.7.9.post39")
+    pdata_version = V("1.8.11.post1")
 except ImportError:
     pass
-data_git_hash = "f165dc22f1f67e3e8bdc8edf750ff7dc596de2ff"
-data_git_describe = "1.7.9-39-gf165dc22f"
+data_git_hash = "16ff442da4b92e28d0753fabed18ad4a15254498"
+data_git_describe = "1.8.11-1-g16ff442da"
 data_git_msg = """\
-commit f165dc22f1f67e3e8bdc8edf750ff7dc596de2ff
-Author: Keith Packard <keithp@keithp.com>
-Date:   Sat Nov 5 19:30:51 2022 -0700
+commit 16ff442da4b92e28d0753fabed18ad4a15254498
+Author: Florent Kermarrec <florent@enjoy-digital.fr>
+Date:   Fri Jun 5 13:13:43 2026 +0200
 
-    Switch read/write to posix types
+    meson: Alias PPC CPU family to PowerPC
     
-    Remove legacy cygwin hacks which set the buffer size and return value
-    for read and write to int, switch them to the correct POSIX types,
-    size_t for buffer size and ssize_t for the return value.
+    Meson uses ppc as the canonical CPU family name for 32-bit PowerPC, while picolibc machine support lives under libc/machine/powerpc. Add the missing alias so cross files using cpu_family='ppc' select the existing PowerPC backend instead of failing with Unsupported architecture: ppc.
     
-    Signed-off-by: Keith Packard <keithp@keithp.com>
+    The 32-bit PowerPC setjmp path and endian macros are already fixed in current picolibc; this addresses the remaining family-name mismatch reported in enjoy-digital/litex#1400.
 
 """
 
